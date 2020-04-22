b2230c73b12041515cdc2a0b0bdcdda58e4553dc
Nilavalagan Sugumaran <>
2020-04-22 10:19:48 +0100

    Comments added

--- topic-five/src/com/example/enums/Example.java
+++ topic-five/src/com/example/enums/Example.java
@@ -2,12 +2,14 @@ package com.example.enums;
 
 public class Example {
 
+    // Example constant
     public static final String NAME = "nila";
 }
 
 class OtherClass{
 
     void print() {
+        //Usage of constant
         System.out.println(Example.NAME);
     }
 }
