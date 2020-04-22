62fc6d7bd1ec51b6672cffe6ea8fac9f3e3cdced
Nilavalagan Sugumaran <>
2020-04-22 15:11:26 +0100

    More lambdaes

--- lambda-examples/src/com/example/lambdas/Lambdamain.java
+++ lambda-examples/src/com/example/lambdas/Lambdamain.java
@@ -2,29 +2,63 @@ package com.example.lambdas;
 
 public class Lambdamain {
 
-    public static void main(String s[]){
+    public static void main(String s[]) {
 
-        //Multi line
-        MyFunctionalInterface<String> stringLambda1 = (p) ->{
-            System.out.println("Input string  is" + p);
-            System.out.println("Length of the string " + p.length());
-
-        };
+        // Multi line
+        MyFunctionalInterface<String> stringLambda1 =
+                (p) -> {
+                    System.out.println("Input string  is" + p);
+                    System.out.println("Length of the string " + p.length());
+                };
         stringLambda1.execute("This is my first String");
 
-        //Single line
-        MyFunctionalInterface<String> stringLambda2 = p -> System.out.println("Length of the string " + p.length());
+        // Single line
+        MyFunctionalInterface<String> stringLambda2 =
+                p -> System.out.println("Length of the string " + p.length());
         stringLambda2.execute("This is my seconds string 11111111");
 
-        MyFunctionalInterface<Person> personLambda = p ->  p.saySomeThing();
+        // Passing object and invoking method
+        MyFunctionalInterface<Person> personLambda = p -> p.saySomeThing();
         personLambda.execute(new Person("my son", 11));
         personLambda.execute(new Person("Nila", 17));
 
+        // Passing multiple parameters and returning value
+        MyOtherFunctionalInterface<Integer> integerLambda =
+                (p, q) -> {
+                    if (p > q) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+
+        System.out.println("is 10 > 20" + integerLambda.executeOther(10, 20));
+        System.out.println("is 30 > 10" + integerLambda.executeOther(30, 10));
+
+        // Passing multiple parameters and returning value
+        MyOtherFunctionalInterface<Person> personComapreLambda =
+                (a, b) -> {
+                    if (a.getAge() > b.getAge()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+
+        System.out.println(
+                "is nila is older then son "
+                        + personComapreLambda.executeOther(
+                                new Person("Nila", 17), new Person("my son", 11)));
+
+        System.out.println(
+                "is nila is older then his dad "
+                        + personComapreLambda.executeOther(
+                                new Person("Nila", 17), new Person("my dad", 65)));
     }
 }
 //
-//class MyClass<T> implements MyFunctionalInterface<T> {
+// class MyClass<T> implements MyFunctionalInterface<T> {
 //    void execute(T a){
 //
 //    }
-//}
+// }
---
+++ lambda-examples/src/com/example/lambdas/MyOtherFunctionalInterface.java
@@ -0,0 +1,8 @@
+package com.example.lambdas;
+
+@FunctionalInterface
+public interface MyOtherFunctionalInterface<T> {
+
+    boolean executeOther(T obj1, T obj2);
+
+}
